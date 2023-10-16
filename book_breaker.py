49c8cd490a09882d109e907e589fee9c493ab5e2
ArtieLiu <yinan97@outlook.com>
2023-10-16 12:43:49 +0800

    add script to split pdf file by page number

--- book_breaker.py
+++ book_breaker.py
@@ -1,31 +1,42 @@
 import os
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
-
-def break_pages_by(break_points):
-    return {
-        "cover1": [1, 2],
-        "preface": [3, 4, 5]
-    }
+PREFIX = 0
 
 
-def write_to_file(name, page_numbers):
-    writer = PdfFileWriter()
-    reader = PdfFileReader(book)
-    for page_number in page_numbers:
-        page = reader.getPage(page_number - 1)
-        writer.addPage(page)
-
-    output_file_path = os.path.join("output", f"{name}.pdf")
+def write(name, cache_writer):
+    global PREFIX
+    output_file_path = os.path.join("output", f"{PREFIX} {name}.pdf")
+    PREFIX += 1
     with open(output_file_path, 'wb') as f:
-        writer.write(f)
+        cache_writer.write(f)
+
+
+def minus_one(number):
+    return number - 1
+
+
+def split_book(book_name, breaks: dict):
+    reader = PdfFileReader(book_name)
+    cache_writer = PdfFileWriter()
+    cache_title = ""
+
+    for page_number in range(1, len(reader.pages) + 1):
+        if page_number in breaks.keys():
+            cache_title = breaks[page_number]
+
+        page = reader.getPage(minus_one(page_number))
+        cache_writer.addPage(page)
+
+        if page_number+1 in breaks.keys():
+            write(cache_title, cache_writer)
+            cache_writer = PdfFileWriter()
 
 
 if __name__ == '__main__':
     book = "visual group theory.pdf"
     break_points = {
         1: "cover1",
-        2: "cover2",
         10: "preface",
         12: "contents",
         16: "overview",
@@ -34,8 +45,4 @@ if __name__ == '__main__':
         304: "index"
     }
 
-    pages_of_parts = break_pages_by(break_points)
-
-    for part_name, numbers in pages_of_parts.items():
-
-        write_to_file(part_name, numbers)
+    split_book(book, break_points)
