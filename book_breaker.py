3b0bc8f5690391da51d5c8ab3d6e3f9582a0bf41
ArtieLiu <yinan97@outlook.com>
2023-10-18 10:45:37 +0800

    save splited book to its own folder

--- book_breaker.py
+++ book_breaker.py
@@ -1,12 +1,29 @@
 import os
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
+book_name = "visual group theory"
+break_points = {
+    1: "cover1",
+    10: "preface",
+    12: "contents",
+    16: "overview",
+    18: "ch1",
+    276: "answers",
+    304: "index"
+}
 PREFIX = 0
 
 
-def write(name, cache_writer):
+def write(name, cache_writer, ):
+    def make_book_dir_if_not_exist():
+        if os.path.isdir(book_name):
+            return
+        else:
+            os.mkdir(book_name)
+
+    make_book_dir_if_not_exist()
     global PREFIX
-    output_file_path = os.path.join("output", f"{PREFIX} {name}.pdf")
+    output_file_path = os.path.join(book_name, f"{PREFIX} {name}.pdf")
     PREFIX += 1
     with open(output_file_path, 'wb') as f:
         cache_writer.write(f)
@@ -16,8 +33,13 @@ def minus_one(number):
     return number - 1
 
 
+def read_pdf(book_name):
+    book_pdf = book_name + '.pdf'
+    return PdfFileReader(book_pdf)
+
+
 def split_book(book_name, breaks: dict):
-    reader = PdfFileReader(book_name)
+    reader = read_pdf(book_name)
     cache_writer = PdfFileWriter()
     cache_title = ""
 
@@ -28,21 +50,10 @@ def split_book(book_name, breaks: dict):
         page = reader.getPage(minus_one(page_number))
         cache_writer.addPage(page)
 
-        if page_number+1 in breaks.keys():
+        if page_number + 1 in breaks.keys():
             write(cache_title, cache_writer)
             cache_writer = PdfFileWriter()
 
 
 if __name__ == '__main__':
-    book = "visual group theory.pdf"
-    break_points = {
-        1: "cover1",
-        10: "preface",
-        12: "contents",
-        16: "overview",
-        18: "ch1",
-        276: "answers",
-        304: "index"
-    }
-
-    split_book(book, break_points)
+    split_book(book_name, break_points)
