c1162813258514a5531b8e505a9c7365d25b92aa
ArtieLiu <yinan97@outlook.com>
2023-11-20 18:00:56 +0800

    fix pypdf

--- book_breaker.py
+++ book_breaker.py
@@ -1,34 +1,36 @@
 import os
 import shutil
 
-from PyPDF2 import PdfFileReader, PdfFileWriter
+from pypdf import PdfReader, PdfWriter
 
-BOOK_NAME = "Kubernetes in Action"
+BOOK_NAME = "Istio in Action"
 BREAKS = {
-    1: "front cover",
-    5: "cover",
-    9: "brief contents",
-    11: "contents",
-    23: "preface",
-    25: "ack",
-    27: "about",
-    33: "part 1",
-    87: "part 2",
-    341: "part 3",
-    566: "appendix",
-    593: "index",
-    627: "back cover"
+    1: "front",
+    4: "cover",
+    8: "brief_contents",
+    10: "detailed_contents",
+    18: "forward",
+    20: "preface",
+    22: "ack",
+    24: "about",
+    30: "p1",
+    104: "p2",
+    294: "p3",
+    346: "p4",
+    430: "appendix",
+    464: "index",
+    480: "back"
 }
 
 
 class Reader:
     def __init__(self, book):
         self.book = book
-        self.pdf_reader = PdfFileReader(book + ".pdf")
+        self.pdf_reader = PdfReader(book + ".pdf")
         self.all_page_numbers = range(1, len(self.pdf_reader.pages) + 1)
 
     def get_page_content(self, page_number):
-        return self.pdf_reader.getPage(page_number - 1)
+        return self.pdf_reader.pages[page_number - 1]
 
     def is_last_page(self, page_number):
         return len(self.pdf_reader.pages) == page_number
@@ -45,14 +47,14 @@ def make_or_empty_dir(folder):
 
 class Writer:
     def __init__(self):
-        self.pdf_writer = PdfFileWriter()
+        self.pdf_writer = PdfWriter()
         self.batch_num = 1
 
     def add_page(self, page):
-        self.pdf_writer.addPage(page)
+        self.pdf_writer.add_page(page)
 
     def clear(self):
-        self.pdf_writer = PdfFileWriter()
+        self.pdf_writer = PdfWriter()
 
     def total_pages(self):
         return len(self.pdf_writer.pages)
