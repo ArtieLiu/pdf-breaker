13fc631fb2c75e62565a3255a549e178bbac959b
ArtieLiu <yinan97@outlook.com>
2024-03-07 11:13:47 +0800

    refactor

--- Writer.py
+++ Writer.py
@@ -15,12 +15,12 @@ class Writer:
     def clear(self):
         self.pdf_writer = PdfWriter()
 
-    def total_pages(self):
+    def get_total_num_of_pages(self):
         return len(self.pdf_writer.pages)
 
     def dump_pages_to(self, title):
-        batch_number = str(self.batch_num).ljust(3)
-        total_pages = str(self.total_pages()).ljust(4)
+        batch_number = format(self.batch_num, width=3)
+        total_pages = format(self.get_total_num_of_pages(), width=4)
         filename = f"{batch_number} {total_pages} {title}.pdf"
 
         output_file_path = os.path.join(self.write_to, filename)
@@ -29,3 +29,7 @@ class Writer:
             self.pdf_writer.write(f)
 
         self.batch_num += 1
+
+
+def format(num, width):
+    return str(num).ljust(width)
